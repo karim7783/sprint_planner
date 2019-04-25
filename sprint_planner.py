3fa16f3180e44c8c2bad817b27f584b4cd0c569c
Karim Ali <karim7783@gmail.com>
2019-04-24 22:02:50 -0500

    Stubbing out methods for datastore class

--- sprint_planner.py
+++ sprint_planner.py
@@ -3,12 +3,24 @@
 ###############
 ### Imports ###
 ###############
+import sqlite3
+###############
+###############
 
-###############
-###############
 class sprint_data_store:
     def __init__(self, *args, **kwargs):
+        self._dbconnection = None
+
+    def create_data_store(self):
         pass
+
+    def enumerate_teams(self):
+        pass
+
+    def spot_data_store(self):
+        pass
+
+
 
 if __name__ == '__main__':
     pass
